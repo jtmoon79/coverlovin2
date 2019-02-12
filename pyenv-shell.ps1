56e67b1800ea991a81a4c1226f092977c81fb270
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2019-02-11 18:39:18 -0800

    Add helper script for starting pyenv shell
    Bonus:
    - decorates window title with virtual environment

---
+++ pyenv-shell.ps1
@@ -0,0 +1,26 @@
+# start pyenv shell within powershell
+
+$ErrorActionPreference = "Stop"
+#Set-PSDebug -Trace 2
+
+# get path to pipenv installed with local python 3.7 installation 
+cd $PSScriptRoot
+$pyver = "3.7"
+$py = "C:\Windows\py.exe"
+Get-ChildItem -Path $py | Out-Null  # assert path exists
+$pipenv = & py "-$pyver" -c "import sys;import os;print(os.path.join(os.path.dirname(sys.executable), 'Scripts', 'pipenv'));"
+
+# update the shell window title with information about the virtual environment
+$title_original = $Host.UI.RawUI.WindowTitle
+$venv_loc = & $pipenv --venv
+$pyexe = & $pipenv --py
+$py_ver = & $pyexe --version
+$Host.UI.RawUI.WindowTitle = $Host.UI.RawUI.WindowTitle + " pipenv: $venv_loc ($py_ver)"
+
+# run `pipenv shell` command
+echo "$pipenv shell"
+echo ""
+& $pipenv "shell"
+
+# restore the window title
+$Host.UI.RawUI.WindowTitle = $title_original
