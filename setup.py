9480909c3ae6707f8e9a779f2d9be13efd975934
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2019-02-27 01:46:39 -0800

    Update setup.py with 'Pillow' requirement

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     install_requires=[
         'musicbrainzngs >= 0.6',
         'mutagen >= 1.42.0',
+        'Pillow >= 5.4.1',
     ],
     # see https://pypi.org/classifiers/
     classifiers=[
