8211d51a75281751715e2f6c0df280e24458b3d4
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2021-02-14 15:21:02 -0800

    NFC pipenv-shell.ps1 use formal Powershell
    Avoid aliases

--- tools/pipenv-shell.ps1
+++ tools/pipenv-shell.ps1
@@ -1,13 +1,13 @@
 # start this project's pipenv shell with decorated window title
 
 $ErrorActionPreference = "Stop"
-#Set-PSDebug -Trace 2
+Set-PSDebug -Trace 1
 
 # get path to pipenv installed with local python 3.7 installation 
-cd $PSScriptRoot
+Set-Location $PSScriptRoot
 $pyver = "3.7"
 $py = "C:\Windows\py.exe"
-Get-ChildItem -Path $py | Out-Null  # assert path exists
+Get-ChildItem -Path $py | Out-Null  # test path exists
 $pipenv = & py "-$pyver" -c "import sys;import os;print(os.path.join(os.path.dirname(sys.executable), 'Scripts', 'pipenv'));"
 
 # update the shell window title with information about the virtual environment
@@ -18,8 +18,8 @@ $py_ver = & $pyexe --version
 $Host.UI.RawUI.WindowTitle = $Host.UI.RawUI.WindowTitle + " pipenv: $venv_loc ($py_ver)"
 
 # run `pipenv shell` command
-echo "$pipenv shell"
-echo ""
+Write-Output "$pipenv shell"
+Write-Output ""
 & $pipenv "shell"
 
 # restore the window title
