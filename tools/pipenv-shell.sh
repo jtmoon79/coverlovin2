df8415468fea2697a00b027de8450e4d9811ab83
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2021-02-20 15:12:03 -0800

    pipenv-shell.sh simplify settings

--- tools/pipenv-shell.sh
+++ tools/pipenv-shell.sh
@@ -13,17 +13,13 @@ ver='3.7'
 python='python'
 pipenv='pipenv'
 
-# attempt to get specific version of python and pipenv
 if which py &>/dev/null; then
     # Windows only
     python=$(py -${ver} -c "import sys;import os;print(os.path.abspath(sys.executable));")
     pipenv=$(py -${ver} -c "import sys;import os;print(os.path.join(os.path.dirname(sys.executable), 'Scripts', 'pipenv'));")
-elif which "python${ver}" &>/dev/null; then
-    python=$(which "python${ver}")
-    pipenv=$(which "pip${ver}")
 elif which python3 &>/dev/null; then
+    # prefer invoking python3
     python=$(which python3)
-    pipenv=$(which pip3)
 fi
 
 echo "${python}"
