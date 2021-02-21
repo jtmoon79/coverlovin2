442d0c22af4f6b5d5078e89daca32e98c9c79f11
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2021-02-20 16:01:22 -0800

    build-install-test.sh selective debug echo of commands

--- tools/build-install-test.sh
+++ tools/build-install-test.sh
@@ -15,17 +15,16 @@ if ! python -B ./tools/is_venv.py &>/dev/null; then
     exit 1
 fi
 
-set -x
 # uninstall any previous install (must be done outside the project directory)
 cd ..
-python -m pip uninstall --yes coverlovin2
+(set -x; python -m pip uninstall --yes coverlovin2)
 # remove previous build artifacts
 rm -rfv ./dist/ ./CoverLovin2.egg-info/
 
 # build using wheels
 cd -
 version=$(python -B -c 'from coverlovin2 import coverlovin2 as c2;print(c2.__version__)')
-python setup.py bdist_wheel
+(set -x; python setup.py bdist_wheel)
 # note the contents of dist
 ls -l ./dist/
 # get the full path to the wheel file
@@ -37,7 +36,7 @@ fi
 
 # install the wheel (must be done outside the project directory)
 cd ..
-python -m pip install "${cv_whl}"
+(set -x; python -m pip install "${cv_whl}")
 
 # make sure to attempt uninstall if asked
 uninstall=false
@@ -46,17 +45,17 @@ if [ "${1+x}" == '--uninstall' ]; then
 fi
 function on_exit(){
     if ${uninstall}; then
-        python -m pip uninstall "coverlovin2"
+        (set -x; python -m pip uninstall "coverlovin2")
     fi
 }
 trap on_exit EXIT
 
 # does it run?
-coverlovin2 --version
+(set -x; coverlovin2 --version)
 
 if ${uninstall}; then
     # and test uninstall if asked
-    python -m pip uninstall "coverlovin2"
+    (set -x; python -m pip uninstall "coverlovin2")
     # if script got here then no need to run uninstall on EXIT
     uninstall=false
 fi
