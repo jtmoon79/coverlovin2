da3a028f600cdd93f40876fe10e1139371b4ae77
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2022-01-20 23:12:16 -0800

    bump 0.7.1

--- coverlovin2/__init__.py
+++ coverlovin2/__init__.py
@@ -6,7 +6,7 @@ __url__ = "https://github.com/jtmoon79/coverlovin2"
 __url_source__ = __url__
 __url_project__ = "https://pypi.org/project/CoverLovin2/"
 __app_name__ = "CoverLovin2"
-__version__ = "0.7.0"
+__version__ = "0.7.1"
 # Product Token should follow RFC #1945, used for Discogs requests
 # https://tools.ietf.org/html/rfc1945#section-3.7
 __product_token__ = __app_name__ + "/" + __version__
