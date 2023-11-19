06b26ad2041cc5d1a9f3625cae85139090159531
James Thomas Moon (@jtmoon79) <815261+jtmoon79@users.noreply.github.com>
2023-11-18 21:36:26 -0800

    __init__.py tweak docstring

--- coverlovin2/__init__.py
+++ coverlovin2/__init__.py
@@ -1,6 +1,7 @@
 # __init__.py
 
 # DRY canonical information
+
 __author__ = "James Thomas Moon"
 __url__ = "https://github.com/jtmoon79/coverlovin2"
 __url_source__ = __url__
@@ -10,6 +11,10 @@ __version__ = "0.7.3"
 # Product Token should follow RFC #1945, used for Discogs requests
 # https://tools.ietf.org/html/rfc1945#section-3.7
 __product_token__ = __app_name__ + "/" + __version__
+"""
+Product Token should follow RFC #1945, used for Discogs requests
+https://tools.ietf.org/html/rfc1945#section-3.7
+"""
 # this __doc__ should be one-line
 __doc__ = """Recursively process passed directories of audio media files, attempting\
  to create a missing album image file, either via local searching and\
