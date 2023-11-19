61607742a911ea5517f51784087bf7f6dfab9d4a
James Thomas Moon (@jtmoon79) <815261+jtmoon79@users.noreply.github.com>
2023-11-18 21:36:44 -0800

    __init__.py version 0.7.4

--- coverlovin2/__init__.py
+++ coverlovin2/__init__.py
@@ -7,9 +7,7 @@ __url__ = "https://github.com/jtmoon79/coverlovin2"
 __url_source__ = __url__
 __url_project__ = "https://pypi.org/project/CoverLovin2/"
 __app_name__ = "CoverLovin2"
-__version__ = "0.7.3"
-# Product Token should follow RFC #1945, used for Discogs requests
-# https://tools.ietf.org/html/rfc1945#section-3.7
+__version__ = "0.7.4"
 __product_token__ = __app_name__ + "/" + __version__
 """
 Product Token should follow RFC #1945, used for Discogs requests
